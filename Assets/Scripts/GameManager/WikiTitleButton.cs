6e7f0ad6e7b525dc425aebfcee43bfbdd4508068
Tempsyche <ruinature7@gmail.com>
2026-01-13 19:17:05 +0800

    按钮选中变色

--- Assets/Scripts/GameManager/WikiTitleButton.cs
+++ Assets/Scripts/GameManager/WikiTitleButton.cs
@@ -1,24 +1,44 @@
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
-using System; // 必须引用这个才能用 Action
+using System;
+using UnityEngine.EventSystems; // 1. 必须引入这个命名空间
 
-public class WikiTitleButton : MonoBehaviour
+// 2. 在这里加上接口声明：IPointerEnterHandler, IPointerExitHandler
+public class WikiTitleButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public Button btn;
     public TextMeshProUGUI titleText;
 
-    // 接收数据，和点击时的回调函数
+    // 保存默认颜色和悬停颜色
+    private Color normalTextColor = Color.white; // 默认白字
+    private Color hoverTextColor = Color.black;  // 悬停黑字
+
     public void Setup(WikiEntryData data, Action<WikiEntryData> onClickAction)
     {
         titleText.text = data.title;
         
-        // 清除旧事件，添加新事件
+        // 确保一开始文字是白色的
+        titleText.color = normalTextColor;
+
         btn.onClick.RemoveAllListeners();
         btn.onClick.AddListener(() => 
         {
-            // 当被点击时，执行传进来的动作，并把自己的数据传回去
             onClickAction(data);
         });
+    }
+
+    // --- 鼠标移入时触发 ---
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if(titleText != null)
+            titleText.color = hoverTextColor; // 变成黑色
+    }
+
+    // --- 鼠标移出时触发 ---
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if(titleText != null)
+            titleText.color = normalTextColor; // 变回白色
     }
 }
